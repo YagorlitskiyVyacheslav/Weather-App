2de5f72f436e9b79746aa5bc3ba649aacf5282d0
Your Name <you@example.com>
2020-09-11 01:03:40 +0300

    templates+murkUpFiveDay

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import fetchImage from './js/fetch-bg-image';
 import refs from './js/refs';
 import quotes from './js/quote';
 import geolocation from './js/geolocation-rendering';
+import markUpFiveDays from './js/markUpFiveDay';
+
 
 navigator.geolocation.getCurrentPosition(
   geolocation.onGetPositionSuccess,
--- src/js/refs.js
+++ src/js/refs.js
@@ -6,4 +6,8 @@ export default {
     tempToday: document.querySelector('.current-temperature-today'),
     tempTodayMin: document.querySelector('.today-min'),
     tempTodayMax: document.querySelector('.today-max'),
+    weatherContainer: document.querySelector('.weatherContainer'),
+    onClickBtnOneDay:document.querySelector('.weather-one-day'),
+    onClickBtnFiveDay:document.querySelector('.weather-5days'),
+
 }
