75ead4eed47e06e94739547c934cf71a5a4e70c4
Vyacheslav <slavasan05@gmail.com>
2020-09-15 20:50:06 +0300

    fix slider

--- src/index.js
+++ src/index.js
@@ -10,14 +10,11 @@ import { onBtnOneDayClick, onBtnFiveDayClick } from './js/markUpFiveDay';
 import renderingCurrentWeather from './js/renderingCurrentWeather';
 import { error, Stack } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
-<<<<<<< HEAD
-=======
 import  'slick-carousel/slick/slick.min';
 import  slick2 from 'slick-carousel';
 require ('jquery');
 import $ from 'jquery';
  window.$ = window.jQuery = $;
->>>>>>> 58eff233249d3799c134d6b203972200f9f82fd1
 import localStorageInput from './js/localStorage';
 import formStar from './js/favorite-sity-star';
 
@@ -33,12 +30,12 @@ refs.onClickBtnFiveDay.addEventListener('click', () => {
       $('.five-days-weather-list').slick({
           arrows:true,
          draggable: false,
-          slidesToShow:6, 
-          slidesToScroll:6,
+          slidesToShow:5, 
+          slidesToScroll:1,
           infinite:false,
           responsive: [
             {breakpoint: 768,settings: { slidesToShow: 3, slidesToScroll: 1 }},
-            { breakpoint: 1024, settings: { slidesToShow: 5, slidesToScroll: 1 } },
+            { breakpoint: 1280, settings: { slidesToShow: 5, slidesToScroll: 1 } },
           ]
       });
       });
@@ -46,8 +43,6 @@ refs.onClickBtnFiveDay.addEventListener('click', () => {
        });
      
 })
-
-
 
 refs.searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -69,13 +64,12 @@ refs.searchForm.addEventListener('submit', (e) => {
         return error({
           text: "Can't show such city!",
         });
-      } else if (data.value === undefined) {
+      } else if (refs.searchFormInput.value === '') {
         return error({
           text: "Please write search city!",
         });
-      } else {
-        return refs.backgroundRef.setAttribute("style", `background-image: url("${data.largeImg}")`);
       }
+      return refs.backgroundRef.setAttribute("style", `background-image: url("${data.largeImg}")`);
     });
   }, 1000)
 })
