3e2281151d224527d2a44a3cfa01e2b08e401378
mazikkk <mazikn19@gmail.com>
2020-09-18 15:19:29 +0300

    timer

--- src/index.js
+++ src/index.js
@@ -12,11 +12,12 @@ import { error, Stack } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
 import { slider1 } from './js/slick-carousel';
 
-import localStorageInput from './js/localStorage';
-import formStar from './js/favorite-sity-star';
+// import localStorageInput from './js/localStorage';
+// import formStar from './js/favorite-sity-star';
 import notification from './js/notification';
 import moment from 'moment';
 moment().format();
+import updateTimer from './js/timer-date';
 
 document.addEventListener('DOMContentLoaded', preloader.start());
 document.addEventListener('DOMContentLoaded', geolocation);
